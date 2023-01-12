88a47b703aa74648e4b7cf7e555eb7c1d074e07c
Leonardo Machado <leona@MacBook-Air.local>
2023-01-12 18:43:39 -0300

    Access Token: tentativa 1

--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,40 @@
 // import Head from "next.head";
 import { useState, useEffect } from "react";
 import { server } from "../config";
+import queryString from "query-string";
+
 function Home({ adobeAPIKey, adobeAPISecret }) {
   const [connected, setconnected] = useState(false);
   const [loading, setLoading] = useState(false);
   const [response, setResponse] = useState(null);
-  const redirectUri = "https://focal.vercel.app";
+  const redirectUri = "https://aspekto.vercel.app";
+  const clientId = adobeAPIKey;
+  const clientSecret = adobeAPISecret;
 
+  const [accessToken, setAccessToken] = useState(null);
+  const [error, setError] = useState(null);
+
+  async function getAccessToken() {
+    const responseCode = await fetch(
+      `https://ims-na1.adobelogin.com/ims/authorize/v2?client_id=${clientId}&state=90cff02f-da33-46ec-985c-1f5cf2f9644a&response_type=code`
+    );
+    console.log(responseCode);
+  }
+
+  // Pegando o status do meu client ID
   async function getHealthStatus() {
     const responseHealth = await fetch(`https://lr.adobe.io/v2/health`, {
       headers: {
-        "x-api-key": `${adobeAPIKey}`,
+        "x-api-key": `${clientId}`,
         // 'Content-Type': 'application/x-www-form-urlencoded',
       },
     });
     console.log(responseHealth.status);
     const responseAuth = await fetch(
-      `https://ims-na1.adobelogin.com/ims/keys`,
-      {
-        // headers: {
-        //   "x-api-key": `${adobeAPIKey}`,
-        //   // 'Content-Type': 'application/x-www-form-urlencoded',
-        // },
-      }
+      `https://ims-na1.adobelogin.com/ims/.well-known/openid-configuration`
     );
     console.log(responseAuth);
   }
-
-  useEffect(() => {
-    const code = new URL(window.location.href).searchParams.get("code");
-
-    // Step 4: Exchange the authorization code for an access token
-    if (code) {
-      fetch("https://ims-na1.adobelogin.com/ims/token", {
-        method: "POST",
-        body: JSON.stringify({
-          grant_type: "authorization_code",
-          client_id: adobeAPIKey,
-          client_secret: clientSecret,
-          code: code,
-          redirect_uri: redirectUri,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((res) => res.json())
-        .then((res) => {
-          const accessToken = res.access_token;
-          console.log(`Your access token is: ${accessToken}`);
-        })
-        .catch((err) => {
-          console.error("An error occurred:", err);
-        });
-    }
-  }, []);
 
   return (
     <main className="pt-24 flex flex-col w-full items-center min-h-screen">
@@ -68,21 +48,26 @@ function Home({ adobeAPIKey, adobeAPISecret }) {
         connect lightroom
       </button>
 
-      <a
-        href={`https://ims-na1.adobelogin.com/ims/authorize?client_id=${adobeAPIKey}&response_type=code&redirect_uri=${redirectUri}`}
+      <button
+        className="flex py-2 px-6 text-gray-700 justify-center border-gray-700 border cursor-pointer text-sm rounded-xl shadow-md hover:text-gray-900 focus:border-gray-900"
+        onClick={
+          async () => (setconnected(true), getAccessToken())
+          // , getHealthStatus()
+        }
       >
-        Login with Adobe
-      </a>
+        Get client authorization
+      </button>
     </main>
   );
 }
 
 export default Home;
 
+// Pegando os IDs do arquivo .env para passar pro componente Home
 export const getStaticProps = async () => {
   require("dotenv").config();
-  const adobeAPIKey = process.env.API_KEY;
-  const adobeAPISecret = process.env.API_SECRET;
+  const adobeAPIKey = process.env.ADOBE_API_KEY;
+  const adobeAPISecret = process.env.ADOBE_API_SECRET;
   return {
     props: {
       adobeAPIKey,
